a45fc9090db26ce0d98d25cc36684ba8244c7b39
Gustavo <al.gustavo.cardoso@impatech.org.br>
2024-08-20 15:16:09 -0300

    teste

---
+++ Malta.py
@@ -0,0 +1,12 @@
+import MYTOOLS as myt
+
+
+print("Imprimindo e com 4 casas")
+myt.e_real(4)
+
+print("Imprimindo pi com 4 casas")
+myt.pi_real(4)
+
+print("referencias")
+print(f"numero e = 3.{myt.E_INT}")
+print(f"numero pi = 3.{myt.PI_INT}")
